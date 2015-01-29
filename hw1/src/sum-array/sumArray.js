04a43c91ea26154126c8b9edc9a23024639d193d
marcpelessone <mwp2117@columbia.edu>
2015-01-28 21:16:28 -0500

    I wrote sumarrays.

--- hw1/src/sum-array/sumArray.js
+++ hw1/src/sum-array/sumArray.js
@@ -2,6 +2,10 @@
   'use strict';
 
   var sumArray = function sumArray(values) {
+    var sum = values.reduce(function(previousValue, currentValue) {
+      return previousValue + currentValue;});
+    return sum;
+
     // IMPLEMENT YOUR SOLUTION HERE!
     // MAKE SURE TO USE REDUCE!
   };
